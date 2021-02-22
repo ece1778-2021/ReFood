c8cdaf59770052953dfb9083326996aa4494cd49
YunyiZhuAlex <yunyi.zhu@mail.utoronto.ca>
2021-02-22 17:14:16 -0500

    Round 1 and 2 of the game done, with local coin counter working.

--- app/src/main/java/com/refood/refood/ExerciseActivity.java
+++ app/src/main/java/com/refood/refood/ExerciseActivity.java
@@ -2,69 +2,88 @@ package com.refood.refood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.res.Resources;
-import android.graphics.Point;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.util.DisplayMetrics;
-import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.view.ViewGroup.LayoutParams;
 
 public class ExerciseActivity extends AppCompatActivity {
 
+    private static final int MAX_LEVEL = 2;
     private final int EXERCISE_DURATION = 10;
-    private int mCount;
+    private int mGameProgress;
 
     private Button mStartButton;
     private ImageView mCue;
+    private TextView mFeedback;
 
     private final int[] mGoCue = {R.drawable.brocolli};
     private final int[] mNoGoCue = {R.drawable.burger};
     private int mParentWidth;
     private int mParentHeight;
+    private int mRound;
+    private int mCoins;
+    private boolean mGoChosen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise);
 
-        // TODO: get the actual number of coins
-        int numCoin = 132;
-        ((TextView)findViewById(R.id.exercise_coin_text)).setText(getString(R.string.coins_template, numCoin));
+        // TODO: get the actual number of coins from firebase
+        mCoins = 0;
+        setCoinCounter();
 
         mCue = findViewById(R.id.cue_view);
         mStartButton = findViewById(R.id.start_exercise_button);
+        mFeedback = findViewById(R.id.feedback_text);
+        mRound = 0;
     }
 
     public void startExercise(View view) {
-        mCount = 0;
-        mStartButton.setVisibility(View.GONE);
-        View parent = (View)mStartButton.getParent();
-        mParentWidth = parent.getWidth() - mCue.getWidth();
-        mParentHeight = parent.getHeight() - mCue.getHeight();
-        startExerciseHepler(view);
+        mGameProgress = 1;
+        mRound++;
+        if (mRound <= MAX_LEVEL)
+        {
+            mStartButton.setVisibility(View.GONE);
+            View parent = (View)mStartButton.getParent();
+            mParentWidth = parent.getWidth() - mCue.getWidth();
+            mParentHeight = parent.getHeight() - mCue.getHeight();
+            startExerciseHelper(view);
+        }
     }
 
-    private void startExerciseHepler(View view) {
+    private void startExerciseHelper(View view) {
         chooseCue();
         new CountDownTimer(2000, 1000) {
             public void onTick(long millisUntilFinished) {
             }
 
             public void onFinish() {
-                mCue.setVisibility(View.INVISIBLE);
-                if (mCount < EXERCISE_DURATION)
+                if (mCue.getVisibility()==View.VISIBLE)
                 {
-                    startExerciseHepler(view);
-                    mCount++;
+                    updateCoinCounter(!mGoChosen);
+                    mCue.setVisibility(View.INVISIBLE);
+                }
+                if (mGameProgress < EXERCISE_DURATION)
+                {
+                    startExerciseHelper(view);
+                    mGameProgress++;
                 }
                 else
                 {
                     mStartButton.setVisibility(View.VISIBLE);
+                    if (mRound+1 > MAX_LEVEL)
+                    {
+                        mStartButton.setText(R.string.game_end_text);
+                    }
+                    else
+                    {
+                        mStartButton.setText(R.string.round_transition_text);
+                    }
                 }
             }
         }.start();
@@ -74,11 +93,35 @@ public class ExerciseActivity extends AppCompatActivity {
     private void chooseCue() {
         mCue.setImageResource(chooseResource(chooseGoCue()));
         mCue.setX(randomInRange(0, mParentWidth));
-        mCue.setY(randomInRange(0, mParentHeight));
+        switch (mRound)
+        {
+            case 1:
+                mCue.setY(randomInRange(0, mParentHeight));
+                break;
+            case 2:
+                mCue.setY(0);
+                ObjectAnimator animation = ObjectAnimator.ofFloat(mCue, "translationY", mParentHeight);
+                animation.setDuration(2000);
+                animation.start();
+                break;
+            default:
+                break;
+        }
         mCue.setVisibility(View.VISIBLE);
     }
     private int[] chooseGoCue() {
-        return (Math.random() < 0.5) ? mGoCue:mNoGoCue;
+        if (Math.random() < 0.5)
+        {
+            mCue.setBackgroundResource(R.drawable.green_border_background);
+            mGoChosen = true;
+            return mGoCue;
+        }
+        else
+        {
+            mCue.setBackgroundResource(R.drawable.red_border_background);
+            mGoChosen = false;
+            return mNoGoCue;
+        }
     }
 
     private int chooseResource(int[] arr)
@@ -89,5 +132,41 @@ public class ExerciseActivity extends AppCompatActivity {
     private int randomInRange(int min, int max)
     {
         return (int)(Math.random() * (max-min) + min);
+    }
+
+    private void updateCoinCounter(boolean increment)
+    {
+        if (increment)
+        {
+            mCoins++;
+            mFeedback.setText(R.string.increment_feedback_text);
+            mFeedback.setTextColor(getColor(R.color.green));
+        }
+        else
+        {
+            mCoins--;
+            mFeedback.setText(R.string.decrement_feedback_text);
+            mFeedback.setTextColor(getColor(R.color.red));
+        }
+        mFeedback.setVisibility(View.VISIBLE);
+        new CountDownTimer(500, 500) {
+            public void onTick(long millisUntilFinished) {
+            }
+
+            public void onFinish() {
+                mFeedback.setVisibility(View.INVISIBLE);
+            }
+        }.start();
+        setCoinCounter();
+    }
+
+    private void setCoinCounter()
+    {
+        ((TextView)findViewById(R.id.exercise_coin_text)).setText(getString(R.string.coins_template, mCoins));
+    }
+
+    public void countClick(View view) {
+        mCue.setVisibility(View.INVISIBLE);
+        updateCoinCounter(mGoChosen);
     }
 }
